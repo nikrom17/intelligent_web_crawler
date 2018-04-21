a8bebdad5df1809d7aedaed9dbef79120cbc2469
Nik <nikrom17@bitbucket.org>
2018-04-21 10:28:35 -0700

    reorg file structure

---
+++ app_clearbit.py
@@ -0,0 +1,9 @@
+# test code to show how ypu can obtain clearbit data
+
+import clearbit
+
+clearbit.key = 'sk_8abade9a563c4a0f41ae1362f0dc1f12'
+
+company = clearbit.Company.find(domain='thecheeseschool.com', stream=True)
+if company is not None:
+    print(company)
---
+++ linkedin_queries.py
@@ -0,0 +1,101 @@
+import re
+import json
+
+from linkedin import linkedin
+from operator import itemgetter
+
+
+def linkedin_api(query, start, chunk):
+    application = linkedin.LinkedInApplication(
+        token='AQXds9U0PuMOTTrEIAn_UU0HGJ6Oy54zIRwqBD8l6zAEHRDGO7gBxzJy7dS7PioiNcP_OnfTgKhDaI6phEodupviTrSDW9zD06ZhtWmz4KnyVdg2qaaBZFShMyrltEK80gyOls1zYr7PuheqK6vjAu7SZ119cIAR4OI_MWAjWODObZ0hBtS8wEGc4oRogkyEmY2ufFncjos9O8_LYjcOSjSN_oAn8A4DzsUhi9ZApxGTPtmgDWXfIy3wQj40BOub4ecwnz45sUqvTR9Qlpq43jMNbgxapJ-ADnEH4uZlQLijBSorurwNBMapn6-2-jhRkR_K2dWPdIiPWg-hAQug9YlBjpUF9w')
+
+    # Query fields: https://developer.linkedin.com/docs/fields/company-profile
+    companies = []
+    tmp_query = application.search_company(
+        selectors=[
+            {
+                'companies': [
+                    'name',
+                    'website-url',
+                    'industries',
+                    'employee-count-range',
+                    'universal-name',
+                    'email-domains',
+                    'specialties',
+                    'locations',
+                    'description',
+                    'status']}],
+        params={
+            'keywords': query,
+            'count': chunk,
+            'start': start,
+            'facet': 'location,us:84',
+            'facet': 'location,us:0'})
+    try:
+        for company in tmp_query["companies"]["values"]:
+            companies.append(dict(company))
+    except Exception:
+        pass
+    return {
+        'data': sort_companies(
+            companies,
+            query),
+        'total': tmp_query["companies"]["_total"]}
+
+
+def sort_companies(companies, query):
+    company_size = {
+        'B': 10,
+        'C': 10,
+        'D': 10,
+        'E': 5,
+        'F': 0,
+        'G': -10,
+        'H': -20,
+        'I': -20}
+    ranked_list = []
+    ordered_list = []
+    parsed_query = re.split('[^0-9A-Za-z\.\-]|(?<!\w)[.]|[.](?!\w)', query)
+    parsed_query = list(filter(None, parsed_query))
+    length = len(parsed_query)
+    for company in companies:
+        val = 0
+        for i in range(length):
+            try:
+                val += company['description'].lower().count(parsed_query[i].lower())
+            except BaseException:
+                pass
+            try:
+                val += company['specialties']['values'].lower().count(parsed_query[i].lower())
+            except BaseException:
+                pass
+            try:
+                val += company['industries']['values'].lower().count(parsed_query[i].lower())
+            except BaseException:
+                pass
+            try:
+                val += company['name'].lower().count(parsed_query[i].lower())
+            except BaseException:
+                pass
+        try:
+            val += company_size[company['employeeCountRange']['code']]
+            if (company_size[company['status']['code']] == 'OOB'):
+                val = -100
+        except BaseException:
+            pass
+        company['ranking'] = val
+    companies.sort(key=lambda x: int(x['ranking']), reverse=True)
+    json.dumps(companies)
+    return companies
+
+
+'''
+def main():
+     query = input("query: ")
+     companies = linkedin_api(query, 0, 20)
+
+
+
+if __name__ == "__main__":
+    main()
+'''
