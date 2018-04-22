5abbe48f42dee39c4c20c1d236782bb1daad3866
Cristian Franco <cfranco@e1z2r7p8.42.us.org>
2018-04-21 20:07:11 -0700

    app.py uupdates

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import requests
 
 from flask_cors import CORS
 from flask import Flask, render_template, jsonify, request
-from srcs.search_utils import scrape_url
 from linkedin import linkedin
 from linkedin_queries import linkedin_api
-from srcs.bing_search_api import search as bing_search
+from srcs.bing_search_api import get_entities_from_search
+from srcs.bing_search_api import scrape_contact_from_url
 from bs4 import BeautifulSoup as bs
 
 app = Flask(__name__, static_url_path='')
@@ -65,8 +65,7 @@ def get_tasks():
 def get_company(name):
     comp = [query for query in query_data["data"]
             if query['universalName'] == name]
-    bzData = getBuzzInfo(comp[0]["name"])
-    print(bzData)
+    bzData = getBuzzInfo(comp[0]["name"],comp[0]["locations"]["values"][0]["address"]["postalCode"])
     comp[0]["websiteUrl"] = (
         "http://" + comp[0]["websiteUrl"],
         comp[0]["websiteUrl"])[
@@ -75,27 +74,46 @@ def get_company(name):
         '^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)',
         comp[0]["websiteUrl"].lower())[1]
     newLInk = 'http://' + domain
-    bingData = bing_search('entities', domain)
-    
-    #browser = webdriver.Chrome(executable_path='./chromedriver')
-    #print(browser.get('http://www.buzzfile.com/Search/Company/Results?searchTerm='+ comp[0]["name"] +'&type=1'))
-    #browser.get('http://www.buzzfile.com/Search/Company/Results?searchTerm='+ comp[0]["name"] +'&type=1')
-    #browser.find_element_by_css_selector('#companyList tr:nth-child(2) td:nth-child(2) a').click()
-    # if 'entities' in bingData:
-    #     print(bingData)
-    # else:
-    #     bingData = bing_search('search', domain)
-    #     print("search")
-    #     print(bingData)
-    return render_template('detail.html', compData=comp[0], buzzData=bzData)
 
-def getBuzzInfo(name):
+    # If you'd like to use the Deep Learning NER model to attempt to 
+    # grab named entities then you can pass 'get_entities=True'
+    # into scrape_contact_from_url. It'll load the model and tensorflow,
+    # return num, emails, links, entities, extras
+    # The model needs to be trained more and it runs slowly so it's off by
+    # default
+    num, emails, links, _, extras = scrape_contact_from_url(domain, get_entities=False)
+    
+    bingData = {}
+    bingData['sources'] = []
+
+    # get social media data and links from Bing
+    for link in links:
+        if 'facebook' in link:
+            bingData['facebook'] = link
+        elif 'twitter' in link:
+            bingData['twitter'] = link
+        elif 'linkedin' in link:
+            bingData['linkedin'] = link
+        else:
+            bingData['sources'].append(link)
+
+    if buzzData.contactPerson:
+        bingData['person'] = get_entities_from_search(buzzData.contactPerson)
+    
+    return render_template('detail.html', compData=comp[0], buzzData=bzData, bingData=bingData)
+
+# look for data on buzzfile page http://www.buzzfile.com/
+def getBuzzInfo(name, postalCode):
 
     data = {}
-    html = requests.get('http://www.buzzfile.com/Search/Company/Results?searchTerm='+ name +'&type=1')
+    link = 'http://www.buzzfile.com/Search/Company/Results?searchTerm='+ name +'&parameter=zipcode--' + postalCode +'&type=1'
+    html = requests.get(link)
     soup = bs(html.text, "lxml")
 
-    searchResult = (soup.select('#companyList tr:nth-of-type(3) td:nth-of-type(2) a')[0]['href'])
+    if ( len(soup.select('#companyList tr:nth-of-type(3) td:nth-of-type(2) a')) == 0 ):
+        return 0
+    else:
+        searchResult = (soup.select('#companyList tr:nth-of-type(3) td:nth-of-type(2) a')[0]['href'])
     data['src'] = 'http://www.buzzfile.com' + searchResult
     cpage = requests.get(data['src'])
     soup = bs(cpage.text, "lxml")
