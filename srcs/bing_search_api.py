5e9bf9fca2a6e9fce3743c3e9baca7af75185b69
Jacob Polloreno <makaveliix16@gmail.com>
2018-04-21 19:27:47 -0700

    add bing search api as main

--- srcs/bing_search_api.py
+++ srcs/bing_search_api.py
@@ -1,6 +1,9 @@
 import click
+import re
 import requests
+import time
 
+from bs4 import BeautifulSoup as bs
 # from config import BING_API_KEY
 # from config import BING_SEARCH_ENDPOINT
 
@@ -34,8 +37,12 @@ BING_SEARCH_MARKET_CODE = {
 }
 
 
-def search(endpoint: str, query: str, market: str='United States/English'):
-    """Searches on the BING API calling a specific endpoint
+def search(endpoint: str='search', query: str=None,
+           market: str='United States/English',
+           search_extras: str= ' AND (intitle:about OR intitle:contact)',
+           param_extras=None,
+           verbose: bool=False):
+    """Searches the BING API calling a specific endpoint
 
     Args:
         endpoint (str): endpoint to request from
@@ -60,14 +67,204 @@ def search(endpoint: str, query: str, market: str='United States/English'):
 
     # Build Request
     headers = {"Ocp-Apim-Subscription-Key": BING_SEARCH_KEY}
-    params = {"q": query}
     if endpoint == 'entities':
-        params['mkt'] = BING_SEARCH_MARKET_CODE[market]
+        params = {"q": query,
+                  "mkt": BING_SEARCH_MARKET_CODE[market]}
+    else:
+        params = {"q": query + search_extras}
+
+    if param_extras:
+        for k, v in param_extras.items():
+            params[k] = v
+
+    if verbose:
+        print('Bing Search...\n')
+        print(f'Query: {query}')
+        print(f'Params: {params}')
 
     # Request
     response = requests.get(
         BING_SEARCH_ENDPOINTS[endpoint], headers=headers, params=params)
 
-    response.raise_for_status()
-    results = response.json()
-    return results
+    if verbose:
+        print(f'Response code: {response}')
+        print(f'{response.text}\n{response.json()}')
+
+    return response.json()
+
+
+def get_entities_from_search(query: str,
+                             market: str='United States/English',
+                             verbose: bool=False):
+    """Return entities from search
+
+    """
+    res = search('entities', query=query, market=market, verbose=verbose)
+
+    if verbose:
+        print("\nSearching entities...\n")
+        print(res)
+
+    return res
+
+    entities = {}
+    try:
+        for entity in res['entities']['value']:
+            info = {}
+            try:
+                info['url'] = entity['url']
+            except Exception:
+                pass
+            try:
+                info['type'] = entity['entityPresentationInfo']['entityTypeHints']
+            except Exception:
+                pass
+            try:
+                info['description'] = entity['description']
+            except Exception:
+                pass
+            entities[entity['name']] = info
+    except BaseException:
+        pass
+
+    return entities
+
+
+def get_urls(query: str, market: str='United States/English'):
+    """Return search links from bing query
+
+    Args:
+
+    Returns:
+        list of links
+    """
+    res = search('search', query, market)
+    links = []
+    try:
+        for page in res['webPages']['value']:
+            links.append(page['url'])
+    except Exception:
+        pass
+
+    # res = search('search', query + " about", market)
+    # try:
+    #     for page in res['webPages']['value']:
+    #         links.append(page['url'])
+    # except Exception:
+    #     pass
+
+    # res = search('search', query + " contact", market)
+    # try:
+    #     for page in res['webPages']['value']:
+    #         links.append(page['url'])
+    # except Exception:
+    #     pass
+
+    if query.startswith('site:'):
+        query = query[5:]
+    time.sleep(.5)
+    res = search('search', f'site:facebook.com {query}',
+                 market,
+                 search_extras='',
+                 param_extras={'count': 1})
+    try:
+        for page in res['webPages']['value']:
+            links.append(page['url'])
+    except Exception:
+        pass
+
+    res = search('search', f'site:twitter.com {query}',
+                 market,
+                 search_extras='',
+                 param_extras={'count': 1})
+    try:
+        for page in res['webPages']['value']:
+            links.append(page['url'])
+    except Exception:
+        pass
+
+    res = search('search', f'site:linkedin.com {query}',
+                 market,
+                 search_extras='',
+                 param_extras={'count': 1})
+    try:
+        for page in res['webPages']['value']:
+            links.append(page['url'])
+    except Exception:
+        pass
+
+    return list(set(links))
+
+
+def scrape_contact_from_url(site: str, market: str='United States/English',
+                            get_entities: bool=False):
+    links = get_urls(f'site:{site}', market=market)
+
+    nums = []
+    emails = []
+    entities = []
+    extras = []
+    for link in links:
+        phone, email, entity = scrape_url(link, get_entities=get_entities)
+        if phone:
+            for num in phone:
+                if num not in nums:
+                    nums.append(num)
+        if email:
+            for e in email:
+                if e not in emails:
+                    emails.append(e)
+        if entity:
+            for e in entity:
+                if e not in entities:
+                    entities.append(e)
+
+    if get_entities:
+        for entity in entities:
+            extras.append(get_entities_from_search(entity, market=market))
+    extras.append(get_entities_from_search(site.split('.')[0], market=market))
+    return nums, emails, links, entities, extras
+
+
+def scrape_url(url: str, get_entities: bool=False):
+    """Scrapes a url for phone, email, and named entities
+
+    """
+    if get_entities:
+        from srcs.ner import run_model
+    try:
+        html = requests.get(url, timeout=3, verify=False)
+    except Exception as e:
+        return [], [], []
+    soup = bs(html.text, "lxml")
+    for script in soup(["script", "style"]):
+        script.decompose()
+    text = soup.get_text()
+
+    links = soup.find_all('a')
+    phone = re.findall(
+        r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})", text)
+    email_regex = re.compile(
+        r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}",
+        re.IGNORECASE)
+    emails = re.findall(email_regex, text)
+
+    if get_entities:
+        entities = [entity["text"]
+                    for entity in run_model(text) if entity['text'] == 'PER']
+
+    for link in links:
+        try:
+            ref = link.get('href')
+            if ref.find('mailto:') > -1:
+                email = re.match(email_regex, ref[7:])
+                if email:
+                    emails.append(email[0])
+            elif ref.find('tel:') > -1:
+                phone.append(ref[4:])
+        except Exception:
+            continue
+
+    if get_entities:
+        return set(phone), set(emails), set(entities)
+    return set(phone), set(emails), []
